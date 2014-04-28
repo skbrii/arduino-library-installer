dc60004a68e1899c71e8bdfadb587feb3ecdb409
Петр Беклемишев <pbeklemsishev@gmail.com>
2014-04-28 16:47:10 +0700

    maybe it work?

--- arduino library installer.bat
+++ arduino library installer.bat
@@ -1,13 +1,42 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 rem | Set this script version
-	set /p %version = "Version 0.01"
+set version=0.05
 rem | Set library for download:
-	set /p %libname = "skbrii/biWheel"
+set libname=skbrii/biWheel
+set lib=biWheel-master
+set libn=biWheel
 rem | Path to Arduino libraries folder:
-	set /p %path_to_ardu_lib_fldr = "C:\Program Files (x86)\Arduino\libraries"
+set ardu_lib_fldr=C:\Program Files (x86)\Arduino\libraries
+rem | Set temp folder:
+set "temp_path=c:\tmp1020"
 
-	
-set /p %www_path_to_lib = "https://github.com/" + %libname + "/archive/master.zip"
+set "_s1=https://github.com/"
+set "_s2=/archive/"
+set "_s3=master.zip"
+set "www_path_to_lib=%_s1%%libname%%_s2%%_s3%"
 
-echo ^Hello, this is library installer script, %version^
-echo 
+set "_d2=master.zip"
+set "temp_file_path=%temp_path%\%_d2%"
+
+echo Hello, this is library installer script, version %version%
+
+rem Creating temp folder
+md %temp_path%
+
+bitsadmin /create myDownloadJob
+bitsadmin /addfile myDownloadJob %www_path_to_lib% %temp_file_path%
+bitsadmin /SetNotifyCmdLine myDownloadJob "%SystemRoot%\system32\bitsadmin.exe" "%SystemRoot%\system32\bitsadmin.exe /complete myDownloadJob"
+bitsadmin /resume myDownloadJob
+
+rem чистим за собой
+sleep 500
+unzip %temp_file_path% -d %temp_path%
+rem del %temp_file_path%
+ren %temp_path%\%lib% %temp_path%\%libn%
+copy %temp_path%\%libn% %ardu_lib_fldr%
+rem rmdir %temp_path%
+bitsadmin /reset
+endlocal
+exit /b
