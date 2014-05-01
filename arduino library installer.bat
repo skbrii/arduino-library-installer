c768c55dcf94cfd7d33057ba077c14715cda6d01
Петр Беклемишев <pbeklemsishev@gmail.com>
2014-05-01 14:30:37 +0700

    now it work!

--- arduino library installer.bat
+++ arduino library installer.bat
@@ -1,12 +1,16 @@
 @echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
+SETLOCAL ENABLEDELAYEDEXPANSION enableextensions
 
 rem | Set this script version
-set version=0.07
+set version=1.0
 rem | Set library for download:
 set git_usr=skbrii
 set git_repo=biWheel
-rem | TEmp path
+rem Or uncomment this 2 lines:
+rem set /p git_usr=
+rem set /p git_repo=
+
+rem | TEmp path, must be on the same disk as Arduino folder
 set "temp_path=c:\tmp1020"
 rem | Path to Arduino libraries folder:
 set ardu_lib_fldr="C:\Program Files (x86)\Arduino\libraries"
@@ -14,8 +18,8 @@ set ardu_lib_fldr="C:\Program Files (x86)\Arduino\libraries"
 
 set "lib=%git_repo%-master"
 set "giturl=https://github.com/"
-set "www_path_to_lib=%giturl%%git_usr%/%git_repo%/archive/%lib%.zip"
-rem https://github.com/skbrii/biWheel/archive/biWheel-master.zip
+set "www_path_to_lib=%giturl%%git_usr%/%git_repo%/archive/master.zip"
+rem https://github.com/skbrii/biWheel/archive/master.zip   biWheel-
 set "temp_file_path=%temp_path%\%lib%.zip"
 set jobName=myDownloadJob
 
@@ -23,27 +27,28 @@ set jobName=myDownloadJob
 echo Hello, this is library installer script, version %version%
 
 rem Creating temp folder
-rem md %temp_path%
+md %temp_path%
 
 rem Magic withs Windows BITSadmin service
 bitsadmin /create %jobName%
 echo %jobName% %www_path_to_lib% %temp_file_path%
 bitsadmin /addfile %jobName% %www_path_to_lib% %temp_file_path%
-rem bitsadmin /SetNotifyCmdLine jobName "%SystemRoot%\system32\bitsadmin.exe" "%SystemRoot%\system32\bitsadmin.exe /complete %jobName%"
 bitsadmin /resume %jobName%
 
 :check
-REM bitsadmin /Info %jobName% > %var1%
 for /F "tokens=*" %%i in ('bitsadmin /Info %jobName%') do set var1=%%i
-echo -------
-echo %var1%
-set "var3='%jobName%' TRANSFERRED 1 / 1"
-set var2=%var1:%var3%=%%
+set "var3='myDownloadJob' TRANSFERRED 1 / 1"
+rem Говнокодищще строкой ниже!
+set var2=%var1:~39,33%
+echo %var2%
 sleep 2
-echo ----
-if NOT "%var1%" == "%var2%" (goto :check) else (bitsadmin /resume %jobName%) 
-echo ++++
-rem sleep 100
+
+if "%var2%" == "%var3%" (goto :ch) else (goto :check)
+:ch
+echo Library downloaded sucessfull!
+rem sleep 10
+bitsadmin /resume %jobName%
+bitsadmin /complete %jobName%
 
 rem Разархивируем полученный архив "biWheel-master.zip", получим папку типа "biWheel-master"
 unzip %temp_file_path% -d %temp_path%
@@ -53,12 +58,23 @@ set "new_temp_lib=%temp_path%\%git_repo%"
 
 rem Переименовываем папку "biWheel-master" в "biWheel"
 ren  %temp_lib% %git_repo%
+echo Renamed %temp_lib% --^> %git_repo%
 rem Копируем ее в папку для библиотек Ардуино-ИДЕ
-xcopy %new_temp_lib% %ardu_lib_fldr%\%git_repo% /E
+xcopy %new_temp_lib% %ardu_lib_fldr%\%git_repo% /E /I
+echo Copied %new_temp_lib% --^> %ardu_lib_fldr%\%git_repo%
 
 rem чистим за собой
-rem rmdir %temp_path%
+rmdir %temp_path% /S /Q
+echo garbage cleaned!
+
 bitsadmin /reset
-rem del %temp_file_path%
+echo +
+echo +
+echo ====================================================
+echo Library %git_usr%/%git_repo% installed sucessfull!
+echo	( or not? 0_o )
+echo		please check!
+echo +
+echo The end!
 endlocal
 exit /b
