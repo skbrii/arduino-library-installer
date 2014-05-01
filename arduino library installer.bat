80c76c6bcde1d49fb59229adbda473143a710cce
Петр Беклемишев <pbeklemsishev@gmail.com>
2014-05-01 11:34:06 +0700

    .

--- arduino library installer.bat
+++ arduino library installer.bat
@@ -6,30 +6,25 @@ set version=0.07
 rem | Set library for download:
 set git_usr=skbrii
 set git_repo=biWheel
+rem | TEmp path
 set "temp_path=c:\tmp1020"
-
-
-rem set "git_repoame=%git_usr%/%git_repo%"
-set "lib=%git_repo%-master"
-rem set git_repo=biWheel
 rem | Path to Arduino libraries folder:
 set ardu_lib_fldr="C:\Program Files (x86)\Arduino\libraries"
-rem | Set temp folder:
-set "temp_path=c:\tmp1020"
 
+
+set "lib=%git_repo%-master"
 set "giturl=https://github.com/"
-set "_s2=/archive/"
 set "master=master.zip"
-set /p "www_path_to_lib=%giturl%%git_usr%/%git_repo%%_s2%%master%"
-
-
+set /p "www_path_to_lib=%giturl%%git_usr%/%git_repo%/archive/%master%"
 set "temp_file_path=%temp_path%\%master%"
+
 
 echo Hello, this is library installer script, version %version%
 
 rem Creating temp folder
 md %temp_path%
 
+rem Magick withs Windows BITSadmin service
 bitsadmin /create myDownloadJob
 bitsadmin /addfile myDownloadJob %www_path_to_lib% %temp_file_path%
 bitsadmin /SetNotifyCmdLine myDownloadJob "%SystemRoot%\system32\bitsadmin.exe" "%SystemRoot%\system32\bitsadmin.exe /complete myDownloadJob"
@@ -38,14 +33,15 @@ bitsadmin /resume myDownloadJob
 rem чистим за собой
 sleep 100
 
+rem Разархивируем полученный архив "master.zip", получим папку типа "biWheel-master"
 unzip %temp_file_path% -d %temp_path%
 
 set "temp_lib=%temp_path%\%lib%"
 set "new_temp_lib=%temp_path%\%git_repo%"
 
-rem echo %temp_lib% %git_repo%
+rem 
 ren  %temp_lib% %git_repo%
-rem echo %new_temp_lib% %ardu_lib_fldr%
+rem 
 xcopy %new_temp_lib% %ardu_lib_fldr%\%git_repo% /E
 
 rmdir %temp_path%
